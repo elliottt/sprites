44e12e7e112ea92d9657b306dc483885f44a2ea1
Trevor Elliott <trevor@galois.com>
2010-07-19 22:01:05 -0700

    Fancy makefile

--- cbits/rules.mk
+++ cbits/rules.mk
@@ -1,10 +1,16 @@
+ifneq "$(V)" ""
+	QUIET_CC	=
+else
+	QUIET_CC	= @echo '   CC   $@';
+endif
+
 CFLAGS	=$(shell sdl-config --cflags) -W
 CC	= gcc
 
 all_cbits : cbits/sdl-opengl.o
 
 cbits/sdl-opengl.o : cbits/sdl-opengl.h cbits/sdl-opengl.c
-	$(CC) $(CFLAGS) -c cbits/sdl-opengl.c -o $@
+	$(QUIET_CC)$(CC) $(CFLAGS) -c cbits/sdl-opengl.c -o $@
 
 clean_cbits :
 	$(RM) cbits/sdl-opengl.o
--- rules.mk
+++ rules.mk
@@ -1,3 +1,13 @@
+ifneq "$(V)" ""
+	QUIET_GHC	=
+	QUIET_LINK	=
+	QUIET_DEPEND	=
+else
+	QUIET_GHC	= @echo '   GHC  $@';
+	QUIET_LINK	= @echo '   LINK $@';
+	QUIET_DEPEND	= @echo '   DEPEND';
+endif
+
 GHC		= ghc -odir=ghc -hidir=ghc -ighc
 HS_SOURCES	= Animation.hs Graphics.hs Position.hs Render.hs \
 		  Sprite.hs Time.hs Event.hs Dungeon.hs
@@ -9,10 +19,10 @@ PACKAGES	= $(addprefix -package ,OpenGL SDL SDL-image stm containers)
 all_Test : ghc ghc/depend Test Draw
 
 Test : cbits/sdl-opengl.o $(OBJS) ghc/Test.o
-	$(GHC) -o $@ $^ $(PACKAGES)
+	$(QUIET_LINK)$(GHC) -o $@ $^ $(PACKAGES)
 
 Draw : cbits/sdl-opengl.o $(OBJS) ghc/Draw.o
-	$(GHC) -main-is Draw -o $@  ghc/Draw.o cbits/sdl-opengl.o $(OBJS) $(PACKAGES)
+	$(QUIET_LINK)$(GHC) -main-is Draw -o $@  ghc/Draw.o cbits/sdl-opengl.o $(OBJS) $(PACKAGES)
 
 ghc :
 	mkdir ghc
@@ -21,20 +31,21 @@ clean_Test :
 	$(RM) -r ghc
 
 ghc/%.o: %.hs
-	$(GHC) -c $<
+	$(QUIET_GHC)$(GHC) -c $<
 
 ghc/%.hi: %.hs
-	$(GHC) -c $<
+	$(QUIET_GHC)$(GHC) -c $<
 
 define main-target
 ghc/$1.o : $1.hs
-	$(GHC) -main-is $1.main -c $1.hs
+	$$(QUIET_GHC)$(GHC) -main-is $1.main -c $1.hs
 
 endef
 
 $(eval $(foreach t,$(TARGETS),$(call main-target,$(t))))
 
 ghc/depend : ghc
-	$(GHC) -M $(HS_SOURCES) $(addsuffix .hs,$(TARGETS)) -dep-makefile $@
+	$(QUIET_DEPEND)$(GHC) -M $(HS_SOURCES) $(addsuffix .hs,$(TARGETS)) \
+		-dep-makefile $@
 
 -include ghc/depend
